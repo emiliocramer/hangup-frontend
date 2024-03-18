cd0352b38d0ddaf8ee29e8e451f9f2c2e62caf3a
emiliocramer <emilio.cramer101@gmail.com>
2024-03-18 14:35:38 -0400

    add v1 logic for number assignment and fetch

--- src/app/components/call/ManageCallSettingsPage.tsx
+++ src/app/components/call/ManageCallSettingsPage.tsx
@@ -10,6 +10,8 @@ export default function ManageCallSettingsPage() {
         opening_line: '',
         closing_line: '',
     })
+    const [activeNumber, setActiveNumber] = useState('');
+    const [selectedNumber, setSelectedNumber] = useState('');
     const [availableNumbers, setAvailableNumbers] = useState([]);
     const [error, setError] = useState('');
     const router = useRouter();
@@ -19,6 +21,15 @@ export default function ManageCallSettingsPage() {
             axios.get(`https://hang-up-c98880200178.herokuapp.com/api/call-settings/${userId}`)
                 .then(response => setCallSettings(response.data))
                 .catch(error => setError('Failed to fetch call settings'));
+
+            axios.get(`https://hang-up-c98880200178.herokuapp.com/api/twilio/get-active-number/${userId}`)
+                .then(response => {
+                    setActiveNumber(response.data.active_number);
+                })
+                .catch(error => {
+                    console.error(error);
+                    setError('Failed to fetch active number');
+                });
 
             axios.get('https://hang-up-c98880200178.herokuapp.com/api/twilio/get-available-numbers')
                 .then(response => setAvailableNumbers(response.data))
@@ -50,6 +61,24 @@ export default function ManageCallSettingsPage() {
         }
     };
 
+    const handleSubmitSelectNumber = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            await axios.put(`https://hang-up-c98880200178.herokuapp.com/api/twilio/assign-number/${userId}`, selectedNumber);
+        } catch (error) {
+            setError('Failed to update call settings');
+        }
+
+        axios.get(`https://hang-up-c98880200178.herokuapp.com/api/twilio/get-active-number/${userId}`)
+            .then(response => {
+                setActiveNumber(response.data.active_number);
+            })
+            .catch(error => {
+                console.error(error);
+                setError('Failed to fetch active number');
+            });
+    };
+
     if (!userId) {
         return (
             <div>
@@ -66,6 +95,19 @@ export default function ManageCallSettingsPage() {
             <h1 className="text-2xl font-bold mb-6 text-center">Manage Your Assistant</h1>
             <form onSubmit={handleSubmit} className="space-y-4">
                 <div className="mb-4">
+                    {activeNumber && (
+                        <div className="mb-4">
+                            <label className="block text-gray-700 font-bold mb-2" htmlFor="active_number">
+                                Active Number
+                            </label>
+                            <div
+                                id="active_number"
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight bg-gray-400 text-gray-600"
+                            >
+                                {activeNumber}
+                            </div>
+                        </div>
+                    )}
                     <label className="block text-gray-700 font-bold mb-2" htmlFor="full_name">
                         Opening Line
                     </label>
@@ -103,14 +145,32 @@ export default function ManageCallSettingsPage() {
                     </Link>
                 </div>
             </form>
-            <div>
+            <form onSubmit={handleSubmitSelectNumber}>
+                <h2 className="text-2xl font-bold mb-4 mt-8">It looks like you dont yet have a number linked to your account. Select one from below to connect your assistant!</h2>
                 <h2 className="text-2xl font-bold mb-4 mt-8">Available Numbers</h2>
-                <ul>
-                    {availableNumbers.map((number: string) => (
-                        <li key={number}>{number}</li>
+                <div className="mb-4">
+                    {availableNumbers.map(({ number, friendlyName }, index) => (
+                        <div key={index} className="flex items-center">
+                            <input
+                                type="radio"
+                                id={number}
+                                name="selectedNumber"
+                                value={number}
+                                onChange={(e) => setSelectedNumber(e.target.value)}
+                                checked={selectedNumber === number}
+                                className="mr-2"
+                            />
+                            <label htmlFor={number}>{friendlyName} ({number})</label>
+                        </div>
                     ))}
-                </ul>
-            </div>
+                </div>
+                <button
+                    type="submit"
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                    Select Number
+                </button>
+            </form>
             {error && <p className="text-red-500 mt-4">{error}</p>}
         </div>
     );
